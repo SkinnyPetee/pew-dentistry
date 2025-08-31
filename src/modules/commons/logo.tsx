9d97de05e904d9c9afad5944a9287b6275993a13
SkinnyPetee <sri321ramhegde@gmail.com>
2025-08-31 18:18:44 +0530

    changed logo

--- src/modules/commons/logo.tsx
+++ src/modules/commons/logo.tsx
@@ -1,15 +1,20 @@
 const Logo = () => {
   return (
     <>
-      <h1 className="font-semibold">
-        <a href="#hero">
+      <a href="#hero">
+        <div className="flex items-center gap-4">
           <img
-            className="w-[60px] h-[60px] object-cover"
-            src="/images/logo/logo.png"
+            className="w-[30px] h-[30px] object-cover"
+            src="/images/logo/logo.jpg"
             alt="logo image"
           />
-        </a>
-      </h1>
+          <p className=" font-bold text-sm">
+            <span className="text-teal-500">P</span>ewsey{" "}
+            <span className="text-teal-500">D</span>ental <br />{" "}
+            <span className="text-teal-500">P</span>ractice
+          </p>
+        </div>
+      </a>
     </>
   );
 };
